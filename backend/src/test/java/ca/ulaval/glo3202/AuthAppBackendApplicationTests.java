3f15a301c170ae36b614e3def15125366af52c28
Émile Savard <emile.savard.21@gmail.com>
2022-01-27 15:55:53 -0500

    Runnable app

--- backend/src/test/java/ca/ulaval/glo3202/AuthAppBackendApplicationTests.java
+++ backend/src/test/java/ca/ulaval/glo3202/AuthAppBackendApplicationTests.java
@@ -1,13 +1,19 @@
 package ca.ulaval.glo3202;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+//@ActiveProfiles("test")
 @SpringBootTest
 class AuthAppBackendApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		assertThat("Test").isEqualTo("Test");
 	}
 
 }
