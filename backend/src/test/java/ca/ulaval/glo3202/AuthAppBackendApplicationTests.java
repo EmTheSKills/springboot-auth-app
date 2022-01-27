c18d68ef47ab8bc32668380b3bbba70bc9baf102
Émile Savard <emile.savard.21@gmail.com>
2022-01-27 14:27:44 -0500

    springboot project init

---
+++ backend/src/test/java/ca/ulaval/glo3202/AuthAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.ulaval.glo3202;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
